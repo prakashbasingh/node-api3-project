af2c376c893035f4112bd87966ecbb7d54953413
unknown <prakashbahadursingh2@gmail.com>
2020-07-09 01:42:49 -0700

    inititial setup complete. in the postrouter file router for get, get by id and deletet post is functional now need some issue to fix the put router

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
+const postsRouter = require("./posts/postRouter.js")
+const usersRouter = require("./users/userRouter.js")
+
 const server = express();
+
+server.use("/api/posts", postsRouter);
+server.use("/api/users", usersRouter);
+
 
 server.get('/', (req, res) => {
   res.send(`<h1> This is my third Backend project </h1>
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const UserDb = require('./userDb.js')
+const PostDb = require('../posts/postDb')
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
   // do your magic!
+  UserDb.insert(req.body)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({ message: "error posting user info"})
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -12,6 +22,18 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', (req, res) => {
   // do your magic!
+  UserDb.get(req.query)
+    .then (users => {
+      if(users) {
+        res.status(200).json(users)
+      } else {
+        res.status(404).json({message: "users are not found"})
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({message: "there is error while retrieving users info"})
+    })
 });
 
 router.get('/:id', (req, res) => {
